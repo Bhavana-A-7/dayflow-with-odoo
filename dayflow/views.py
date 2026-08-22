2d69585f213120f0a8ef0c59eb52f6abbe6de323
Bhavana <bhavanaha0407@gmail.com>
2026-08-22 10:13:57 +0530

    Add DayFlow REST API

--- dayflow/views.py
+++ dayflow/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Department, Employee, Attendance, Leave
+from .serializers import (
+    DepartmentSerializer,
+    EmployeeSerializer,
+    AttendanceSerializer,
+    LeaveSerializer,
+)
+
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class AttendanceViewSet(viewsets.ModelViewSet):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+
+
+class LeaveViewSet(viewsets.ModelViewSet):
+    queryset = Leave.objects.all()
+    serializer_class = LeaveSerializer
